0b25dfbd2a5d252988e6ac1b10f28d4ada79b488
velamala <>
2018-11-01 19:21:21 -0600

    list display

--- js-list-display-challenge/js/main.js
+++ js-list-display-challenge/js/main.js
@@ -4,3 +4,20 @@ var cities = [
   "Black Hawk",
   "Aurora"
 ];
+
+ for (let i = 0; i < cities.length; i++) {
+   var newListEl = document.createElement("li");
+   let city = newListEl.append(cities[i]);
+   $( ".list-cities" ).append(newListEl);
+ }
+
+ // let cityList= document.querySelector("ul.list-cities");
+
+
+// var list = document.querySelector('ul.list-cities');
+//
+// for (var i = 0; i < cities.length; i++) {
+//   listItem = document.createElement('li');
+//   listItem.innerText = cities[i];
+//   list.append(listItem);
+// }
